31c96a17a0b4721ed7812e18e7ff5c86633ff3f9
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2024-09-16 21:56:53 +0200

    Adds some documentation to Keyframes

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/triggers/utils/intensity/Keyframes.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/triggers/utils/intensity/Keyframes.kt
@@ -15,6 +15,12 @@ class Keyframes(
         )
     }
 
+    /**
+     * @param position the proportional progress on the trigger at which this
+     *    keyframe is located (so 0 is at the beginning of the trigger and 1 is
+     *    at the end)
+     * @param value proportional value
+     */
     data class Keyframe(val position: Double, val value: Double) {
         fun relativeSecondPosition(placedTrigger: AbstractPlacedTrigger) = position * placedTrigger.duration
         fun absoluteSecondPosition(placedTrigger: AbstractPlacedTrigger) = placedTrigger.startTime + position * placedTrigger.duration
