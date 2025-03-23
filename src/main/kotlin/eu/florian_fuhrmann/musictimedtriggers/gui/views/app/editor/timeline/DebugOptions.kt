ff1d38dd22a67c541a0df4c682af869139f05af6
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-03-23 18:19:36 +0100

    removes/disables timeline debug code

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/editor/timeline/DebugOptions.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/editor/timeline/DebugOptions.kt
@@ -2,7 +2,7 @@ package eu.florian_fuhrmann.musictimedtriggers.gui.views.app.editor.timeline
 
 object DebugOptions {
 
-    const val DRAW_TIME = true
+    const val DRAW_TIME = false
     var drawBeginTime: Long = 0L
     const val IMAGE_PARTS = false
 
--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/editor/timeline/EditorTimeline.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/editor/timeline/EditorTimeline.kt
@@ -103,8 +103,7 @@ fun EditorTimeline() {
                                 DebugOptions.drawBeginTime = System.currentTimeMillis()
                             }
                             // render timeline
-                            val inset = 35
-                            TimelineRenderer.render(g as Graphics2D, inset, inset, this.width - 2*inset, this.height - 2*inset)
+                            TimelineRenderer.render(g as Graphics2D, 0, 0, this.width, this.height)
                             // draw finished
                             if(DebugOptions.DRAW_TIME) {
                                 println("Draw took ${System.currentTimeMillis() - DebugOptions.drawBeginTime}ms")
