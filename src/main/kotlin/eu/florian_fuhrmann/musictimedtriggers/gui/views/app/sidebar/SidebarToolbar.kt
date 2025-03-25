00816ed3cf39bb70c064220e240b5f9763ee3479
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-03-25 17:04:03 +0100

    removes old code from SidebarToolbar

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/sidebar/SidebarToolbar.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/sidebar/SidebarToolbar.kt
@@ -5,15 +5,12 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import eu.florian_fuhrmann.musictimedtriggers.gui.dialogs.DialogManager
 import eu.florian_fuhrmann.musictimedtriggers.gui.dialogs.editsong.EditSongDialog
 import eu.florian_fuhrmann.musictimedtriggers.gui.dialogs.editsong.openDeleteSongDialog
-import eu.florian_fuhrmann.musictimedtriggers.gui.views.components.SimpleIconButton
 import eu.florian_fuhrmann.musictimedtriggers.project.ProjectManager
-import eu.florian_fuhrmann.musictimedtriggers.utils.icons.MttIcons
 import org.jetbrains.jewel.foundation.theme.JewelTheme
 import org.jetbrains.jewel.ui.component.Icon
 import org.jetbrains.jewel.ui.component.IconButton
@@ -48,41 +45,5 @@ fun SidebarToolbar() {
         ) {
             Icon(AllIconsKeys.General.Remove, null)
         }
-//        SimpleIconButton(
-//            modifier = Modifier.aspectRatio(1f),
-//            forceHoverHandCursor = true,
-//            iconKey =  MttIcons.plus,
-//            onClick = {
-//                DialogManager.openDialog(EditSongDialog(project = project, add = true))
-//            }
-//        )
-//        SimpleIconButton(
-//            modifier = Modifier.aspectRatio(1f),
-//            forceHoverHandCursor = true,
-//            iconKey =  MttIcons.minus,
-//            enabled = project.currentSong != null,
-//            onClick = {
-//                openDeleteSongDialog(project, project.currentSong)
-//            }
-//        )
-//        SimpleIconButton(
-//            modifier = Modifier.aspectRatio(1f),
-//            forceHoverHandCursor = true,
-//            iconKey =  MttIcons.editBox,
-//            enabled = ProjectManager.currentProject?.currentSong != null,
-//            onClick = {
-//                if(ProjectManager.currentProject!!.currentSong != null) {
-//                    //open song edit dialog
-//                    DialogManager.openDialog(
-//                        EditSongDialog(
-//                            project = project,
-//                            add = false,
-//                            song = project.currentSong!!
-//                        )
-//                    )
-//                }
-//            }
-//        )
-//        Spacer(modifier = Modifier.weight(1f))
     }
 }
