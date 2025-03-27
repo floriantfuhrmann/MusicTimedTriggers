a17ee9b2fb938b183c4723d6849898746f69c9c7
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-03-27 14:11:02 +0100

    automatically focuses cancel button for alerts

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/alerts/BasicAlert.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/alerts/BasicAlert.kt
@@ -5,8 +5,12 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -69,8 +73,12 @@ class BasicAlert(
 
     @Composable
     override fun CancelButton(onClick: () -> Unit, label: String) {
-        OutlinedButton(onClick) {
+        val focusRequester = remember { FocusRequester() }
+        OutlinedButton(onClick, Modifier.focusRequester(focusRequester)) {
             Text(label)
+        }
+        LaunchedEffect(Unit) {
+            focusRequester.requestFocus()
         }
     }
 
