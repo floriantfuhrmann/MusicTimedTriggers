dbd9ea6086b20a395c24037b1f928269bdae7535
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-04-04 14:51:11 +0200

    removes old edit/add-song dialog

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/utils/audio/AudioHelper.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/utils/audio/AudioHelper.kt
@@ -5,21 +5,6 @@ import javax.sound.sampled.AudioFormat
 import javax.sound.sampled.AudioSystem
 import javax.sound.sampled.UnsupportedAudioFileException
 
-@Deprecated("Use ConvertAudioDialog instead.")
-val AVAILABLE_CODECS: List<Codec> = listOf(
-    Codec("pcm_s8", "PCM signed 8-bit", false),
-    Codec("pcm_s16be", "PCM signed 16-bit big-endian", true),
-    Codec("pcm_s16le", "PCM signed 16-bit little-endian", false),
-    Codec("pcm_s24be", "PCM signed 24-bit big-endian", true),
-    Codec("pcm_s24le", "PCM signed 24-bit little-endian", false),
-    Codec("pcm_s32be", "PCM signed 32-bit big-endian", true),
-    Codec("pcm_s32le", "PCM signed 32-bit little-endian", false),
-    Codec("pcm_s64be", "PCM signed 64-bit big-endian", true),
-    Codec("pcm_s64le", "PCM signed 64-bit little-endian", false)
-)
-@Deprecated("Use ConvertAudioDialog instead.")
-val AVAILABLE_SAMPLE_RATES: List<Int> = listOf(11025, 22050, 44100)
-
 fun getAudioFormat(audioFile: File): AudioFormat? {
     try {
         val audioInputStream = AudioSystem.getAudioInputStream(audioFile)
