091df5d583e6ea4c4ee0cd90a1a09995b06dbee5
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-04-18 14:56:15 +0200

    fixes background color of songs in the sidebar

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/uistate/MainUiState.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/uistate/MainUiState.kt
@@ -1,11 +1,14 @@
 package eu.florian_fuhrmann.musictimedtriggers.gui.uistate
 
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import eu.florian_fuhrmann.musictimedtriggers.project.ProjectManager
+import org.jetbrains.jewel.foundation.theme.JewelTheme
+import org.jetbrains.jewel.ui.theme.colorPalette
 import org.jetbrains.skiko.SystemTheme
 import org.jetbrains.skiko.currentSystemTheme
 
@@ -49,21 +52,20 @@ enum class Theme(val displayName: String) {
     fun isDark() =
         (if (this == System) fromSystemTheme(currentSystemTheme) else this) == Dark
 
-    fun highlightGray(): Color = if (isDark()) {
-        Color.LightGray
-    } else {
-        Color.DarkGray
-    }
-    fun errorTextColor(): Color = if (isDark()) {
-        Color(224, 0, 0)
-    } else {
-        Color.Red
-    }
     fun iconColor(): Color = if (isDark()) {
         Color(255, 255, 255)
     } else {
         Color(85, 85, 85)
     }
+
+    /** Secondary Background color for selected items (for example, in the sidebar) */
+    val secondarySelectedBackgroundColor
+        @Composable
+        get() = if (MainUiState.theme.isDark()) {
+            JewelTheme.colorPalette.blue(2)
+        } else {
+            JewelTheme.colorPalette.blue(11)
+        }
 
     companion object {
         fun fromSystemTheme(systemTheme: SystemTheme) =
