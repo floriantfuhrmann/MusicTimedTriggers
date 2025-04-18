18f0bf8854a77334c9c6f9fc953925f7d40e9ae8
Florian Fuhrmann <mail@florian-fuhrmann.eu>
2025-04-18 16:34:07 +0200

    adds padding to inspector bar items

--- src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/inspector/InspectorBar.kt
+++ src/main/kotlin/eu/florian_fuhrmann/musictimedtriggers/gui/views/app/inspector/InspectorBar.kt
@@ -10,13 +10,9 @@ import eu.florian_fuhrmann.musictimedtriggers.gui.uistate.MainUiState
 import eu.florian_fuhrmann.musictimedtriggers.utils.icons.MttIcons
 import org.jetbrains.jewel.foundation.modifier.trackActivation
 import org.jetbrains.jewel.foundation.theme.JewelTheme
-import org.jetbrains.jewel.ui.component.SelectableIconActionButton
-import org.jetbrains.jewel.ui.icons.AllIconsKeys
+import org.jetbrains.jewel.ui.component.Icon
+import org.jetbrains.jewel.ui.component.SelectableIconButton
 import org.jetbrains.jewel.ui.painter.hints.Size
-
-private fun selectOption(option: InspectorOption) {
-    MainUiState.inspectorOption = if(MainUiState.inspectorOption == option) InspectorOption.None else option
-}
 
 @Composable
 fun InspectorBar() {
@@ -26,34 +22,37 @@ fun InspectorBar() {
         .background(JewelTheme.globalColors.borders.normal)
         .padding(start = 1.dp)
         .background(JewelTheme.globalColors.panelBackground)
-        .padding(5.dp)
+        .padding(5.dp),
+        verticalArrangement = Arrangement.spacedBy(5.dp)
     ) {
-        SelectableIconActionButton(
-            key = MttIcons.song,
-            contentDescription = null,
-            onClick = { selectOption(InspectorOption.Song) },
-            extraHint = Size(20),
-            selected = MainUiState.inspectorOption == InspectorOption.Song,
-            focusable = false,
-            modifier = Modifier.trackActivation()
-        )
-        SelectableIconActionButton(
-            key = MttIcons.placedTrigger,
-            contentDescription = null,
-            onClick = { selectOption(InspectorOption.PlacedTrigger) },
-            extraHint = Size(20),
-            selected = MainUiState.inspectorOption == InspectorOption.PlacedTrigger,
-            focusable = false,
-            modifier = Modifier.padding(top = 5.dp).trackActivation()
-        )
-        SelectableIconActionButton(
-            key = MttIcons.triggerTemplate,
-            contentDescription = null,
-            onClick = { selectOption(InspectorOption.TriggerTemplate) },
-            extraHint = Size(20),
-            selected = MainUiState.inspectorOption == InspectorOption.TriggerTemplate,
-            focusable = false,
-            modifier = Modifier.padding(top = 5.dp).trackActivation()
+        InspectorItemButton(InspectorOption.Song)
+        InspectorItemButton(InspectorOption.PlacedTrigger)
+        InspectorItemButton(InspectorOption.TriggerTemplate)
+    }
+}
+
+private fun selectOption(option: InspectorOption) {
+    MainUiState.inspectorOption = if(MainUiState.inspectorOption == option) InspectorOption.None else option
+}
+
+@Composable
+private fun InspectorItemButton(inspectorOption: InspectorOption) {
+    SelectableIconButton(
+        onClick = { selectOption(inspectorOption) },
+        selected = MainUiState.inspectorOption == inspectorOption,
+        focusable = false,
+        modifier = Modifier.trackActivation()
+    ) {
+        Icon(
+            key = when (inspectorOption) {
+                InspectorOption.Song -> MttIcons.song
+                InspectorOption.PlacedTrigger -> MttIcons.placedTrigger
+                InspectorOption.TriggerTemplate -> MttIcons.triggerTemplate
+                else -> error("Unknown inspector option $inspectorOption")
+            },
+            contentDescription = "${inspectorOption.name} Inspector Item",
+            hint = Size(20),
+            modifier = Modifier.padding(5.dp),
         )
     }
 }
